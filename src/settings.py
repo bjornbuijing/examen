7e9fd049a4b61b44acfd67bdb6a6b69436e07a8e
R.Grouls <raoul.grouls@businessdecision.nl>
2022-06-10 17:00:39 +0200

    settings

--- src/settings.py
+++ src/settings.py
@@ -11,7 +11,9 @@ class EurosatSettings(BaseSettings):
     data_dir = Path("../data/raw")
     valid_paths = Path("ds")
 
+
 class StyleSettings(BaseSettings):
     data_dir = Path("../data/external")
-    trainpath = Path('../data/external/sentences/train.feather')
-    testpath = Path('../data/external/sentences/test.feather')
+    trainpath = Path("../data/external/sentences/train.feather")
+    testpath = Path("../data/external/sentences/test.feather")
+    log_dir = Path("../tune")
