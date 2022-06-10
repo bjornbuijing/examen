64a2f032d6fc3c63d85c5e7fd37a13a8453a07b7
R.Grouls <raoul.grouls@businessdecision.nl>
2022-06-10 15:41:58 +0200

    settings

--- src/settings.py
+++ src/settings.py
@@ -1,9 +1,17 @@
-from pydantic import BaseModel
 from pathlib import Path
+
+from pydantic import BaseModel
+
 
 class BaseSettings(BaseModel):
     data_dir: Path
 
+
 class EurosatSettings(BaseSettings):
     data_dir = Path("../data/raw")
     valid_paths = Path("ds")
+
+class StyleSettings(BaseSettings):
+    data_dir = Path("../data/external")
+    trainpath = Path('../data/external/sentences/train.feather')
+    testpath = Path('../data/external/sentences/test.feather')
